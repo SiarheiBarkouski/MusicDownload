f68d2a5150b1bf854a7c5b836453d735f39892cc
serega1993 <serega_10.10.93@mail.ru>
2017-08-22 23:34:39 +0300

    for private use only(small xamarin app)

---
+++ MusicDownload/Classes/MyWebViewClient.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Android.App;
+using Android.Widget;
+using Android.Webkit;
+using System.Net;
+using System.IO;
+
+namespace MusicDownload.Classes
+{
+
+    public class MyWebViewClient : WebViewClient
+    {
+        Action showAlert;
+        ProgressBar pb;
+        private bool isUrlCreated;
+        private string urlCreated;
+
+
+        public MyWebViewClient(Action showAlert, ProgressBar pb)
+        {
+            isUrlCreated = false;
+            this.showAlert = showAlert;
+            this.pb = pb;
+        }
+
+        public string UrlCreated { get => urlCreated; private set => urlCreated = value; }
+
+        public bool IsUrlCreated { get => isUrlCreated; }
+
+        public override WebResourceResponse ShouldInterceptRequest(WebView view, IWebResourceRequest request)
+        {
+            var url = request.Url.ToString();
+
+            if (url.Contains(".mp3?extra"))
+            {
+                int indexMP3 = url.IndexOf(".mp3");
+                url = url.Substring(0, indexMP3 + 4);
+
+                if (url != null)
+                {
+                    urlCreated = url;
+                    isUrlCreated = true;
+                }
+            }
+            return null;
+        }       
+    }
+
+}
+
+
+
---
+++ MusicDownload/MainActivity.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Webkit;
+using System.Net;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using MusicDownload.Classes;
+
+namespace MusicDownload
+{
+    [Activity(Label = "MusicDownload", MainLauncher = true, Icon = "@drawable/icon")]
+    public partial class MainActivity : Activity
+    {
+        string mainUrl;
+        ProgressBar pb;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            mainUrl = @"http://vk.com/audio";
+
+            RequestWindowFeature(WindowFeatures.NoTitle);
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            pb = FindViewById<ProgressBar>(Resource.Id.progressBar1);
+            Button y = FindViewById<Button>(Resource.Id.button1);
+
+
+            WebView x = FindViewById<WebView>(Resource.Id.webView1);
+            x.Settings.JavaScriptCanOpenWindowsAutomatically = true;
+            x.Settings.JavaScriptEnabled = true;
+            MyWebViewClient a = new MyWebViewClient(ShowAlertHandler, pb);
+            
+            x.SetWebViewClient(a);
+            x.LoadUrl(mainUrl);            
+            
+            try
+            {
+                y.Click += (os, ea) =>
+                {
+                    if (a.IsUrlCreated)
+                    {                        
+                        StartDownloadAsync(a.UrlCreated, ShowAlertHandler, pb);
+                    }
+                };
+            }
+            catch (Exception ex)
+            {
+                Toast.MakeText(this, ex.Message, ToastLength.Long).Show();
+            }            
+        }
+
+        async void StartDownloadAsync(string url, Action showAlert, ProgressBar pb)
+        {
+            pb.Progress = 0;
+            Progress<DownloadBytesProgress> progressReporter = new Progress<DownloadBytesProgress>();
+            progressReporter.ProgressChanged += (s, args) => pb.Progress = (int)(100 * args.PercentComplete);
+
+            Task downloadTask = CreateDownloadTask(url, showAlert, pb, progressReporter);
+            await downloadTask;
+        }
+        
+        public static async Task CreateDownloadTask(string url, Action showAlert, ProgressBar pb, IProgress<DownloadBytesProgress> progessReporter)
+        {
+            string pattern = @"(?<=\/)\w*(?=.mp3)";
+            Regex r = new Regex(pattern);
+            var x = r.Match(url).Value;
+
+            WebClient wc = new WebClient();
+            wc.DownloadProgressChanged += (s, ea) =>
+            {
+                if (ea.BytesReceived != 0 && progessReporter != null)
+                {
+                    DownloadBytesProgress args = new DownloadBytesProgress(url, ea.BytesReceived, ea.TotalBytesToReceive);
+                    progessReporter.Report(args);
+                }
+            };
+            wc.DownloadFileCompleted += (os, ea) => { showAlert.Invoke(); };
+            await wc.DownloadFileTaskAsync(new Uri(url), Path.Combine(Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryMusic).Path, $"{x}.mp3"));
+        }
+
+        public void ShowAlertHandler()
+        {            
+            if (Looper.MyLooper() == null)
+                Looper.Prepare();
+            RunOnUiThread(ShowAlert);
+        }
+
+        public void ShowAlert()
+        {
+            Toast.MakeText(this, "File downloaded", ToastLength.Long).Show();
+        }
+
+    }
+}
+
+
